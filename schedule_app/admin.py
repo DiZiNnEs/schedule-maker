e9fb6359cb833499d0ba1d876f6227541d6ee705
dizinnes <dizinnes228@gmail.com>
2021-05-15 16:07:38 +0600

    feat(app/admin): Connect models to admin panel

--- schedule_app/admin.py
+++ schedule_app/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from schedule_app.models import (
+    Chief,
+    Worker,
+    Category,
+    Schedule,
+    Task,
+)
+
+
+@admin.register(Chief)
+class ChiefAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Worker)
+class WorkerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Schedule)
+class ScheduleAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
