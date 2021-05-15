dd84a8e70aa154f8fd1d187caadce0efa443cff4
dizinnes <dizinnes228@gmail.com>
2021-05-15 19:47:08 +0600

    refactor(app/models): Add string method for class

--- schedule_app/models.py
+++ schedule_app/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
 
+    def __str__(self):
+        return self.name
+
 
 class Chief(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -19,6 +22,9 @@ class Chief(models.Model):
         db_table = 'chief'
         verbose_name = 'chief'
         verbose_name_plural = 'chiefs'
+
+    def __str__(self):
+        return f'Chief {self.user.username}'
 
 
 class Worker(models.Model):
@@ -31,6 +37,9 @@ class Worker(models.Model):
         verbose_name = 'worker'
         verbose_name_plural = 'workers'
 
+    def __str__(self):
+        return f'Worker {self.user.username}'
+
 
 class Schedule(models.Model):
     name = models.CharField(max_length=256)
@@ -41,6 +50,9 @@ class Schedule(models.Model):
         verbose_name = 'schedule'
         verbose_name_plural = 'schedules'
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     title = models.CharField(max_length=128)
@@ -49,3 +61,5 @@ class Task(models.Model):
     class Meta:
         db_table = 'task'
 
+    def __str__(self):
+        return self.title
