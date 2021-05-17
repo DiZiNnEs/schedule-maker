1e422cc618888cc7881157faf8cc96df7ad4bc3d
dizinnes <dizinnes228@gmail.com>
2021-05-17 21:37:47 +0600

    feat(views): Create index page

--- schedule_app/views.py
+++ schedule_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = 'schedule_app/index.html'
+    login_url = 'auth/login/'
