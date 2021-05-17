485cb9f616f0e38410ed018dcc10f40359582486
dizinnes <dizinnes228@gmail.com>
2021-05-17 21:39:47 +0600

    refactor(project/settings): Create indent for `INSTALLED_APPS`

--- schedule_maker/settings.py
+++ schedule_maker/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'schedule_app.apps.ScheduleAppConfig',
+    'authorization.apps.AuthorizationConfig',
 ]
 
 MIDDLEWARE = [
