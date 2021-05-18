2690ed035cf9892c3b05d72557321865cac3b12d
dizinnes <dizinnes228@gmail.com>
2021-05-18 23:36:43 +0600

    feat(app/auth): Complete system authentication

--- authorization/views.py
+++ authorization/views.py
@@ -1,17 +1,43 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import generic
-
+from django.views.generic.edit import CreateView
 from django.contrib.auth import (
     views,
     forms,
-    logout
+    logout,
+    authenticate,
+    login,
 )
 
+from django.views import View
 
-class LoginView(views.LoginView):
-    form_class = forms.AuthenticationForm
+from authorization.forms import LoginForm
+
+
+class LoginView(View):
+    form_class = LoginForm
     template_name = 'authentication/login.html'
 
+    def get(self, request, *args, **kwargs):
+        context = {'form': self.form_class}
+        return render(request, self.template_name, context)
 
-class RegistrationView(views.FormView):
+    def post(self, request, *args, **kwargs):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return HttpResponse('Invalid data')
+
+
+class RegistrationView(CreateView):
     form_class = forms.UserCreationForm
     template_name = 'authentication/registration.html'
+
+    def get_success_url(self):
+        return reverse('login')
