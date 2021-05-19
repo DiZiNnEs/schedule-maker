ebc356476c614d69e6320f3e794987a00813a459
dizinnes <dizinnes228@gmail.com>
2021-05-19 23:46:53 +0600

    feat(app/views): Add basic output schedule

--- schedule_app/views.py
+++ schedule_app/views.py
@@ -1,8 +1,18 @@
-from django.views.generic import TemplateView
+from django.views.generic.list import ListView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from django.shortcuts import get_object_or_404, get_list_or_404
 
-class IndexView(LoginRequiredMixin, TemplateView):
+from schedule_app.models import Schedule
+
+
+class IndexView(LoginRequiredMixin, ListView):
+    model = Schedule
     template_name = 'schedule_app/index.html'
     login_url = 'auth/login/'
+
+    def get_context_data(self, object_list=None, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = get_object_or_404(self.model, workers__user__first_name=self.request.user.first_name)
+        return context
