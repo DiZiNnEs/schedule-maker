5f3c984c0e89b1e6aefbdd1da62c7ca42259bfc2
dizinnes <dizinnes228@gmail.com>
2021-05-20 01:06:25 +0600

    refactor(templates): Add name, task name and workers of schedule for authorization user

--- schedule_app/views.py
+++ schedule_app/views.py
@@ -2,7 +2,7 @@ from django.views.generic.list import ListView
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import get_list_or_404
 
 from schedule_app.models import Schedule
 
@@ -14,5 +14,5 @@ class IndexView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, object_list=None, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tasks'] = get_object_or_404(self.model, workers__user__first_name=self.request.user.first_name)
+        context['tasks'] = get_list_or_404(self.model, workers__user__first_name=self.request.user.first_name)
         return context
