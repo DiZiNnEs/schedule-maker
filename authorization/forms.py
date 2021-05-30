412ce418dea9a663f08453230f8741b59ca88aa4
dizinnes <dizinnes228@gmail.com>
2021-05-30 17:48:03 +0600

    refactor(app/forms): Add widgets for fields in forms.py

--- authorization/forms.py
+++ authorization/forms.py
@@ -1,9 +1,13 @@
-from django.forms import ModelForm
+from django import forms
 
 from django.contrib.auth.models import User
 
 
-class LoginForm(ModelForm):
+class LoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'password')
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'password': forms.TextInput(attrs={'class': 'form-control'}),
+        }
